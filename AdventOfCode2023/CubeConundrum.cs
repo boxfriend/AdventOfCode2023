9392fa17be2e2ead1bfdd6f159d025cad57d9a49
boxfriend <cawills394@gmail.com>
2023-12-02 00:40:20 -0500

    day two part one

---
+++ AdventOfCode2023/CubeConundrum.cs
@@ -0,0 +1,91 @@
+﻿namespace AdventOfCode2023;
+
+public class CubeConundrum : IAdventSolution
+{
+    public static string Evaluate (string[] data)
+    {
+        var sum = 0;
+        foreach(var game in data)
+        {
+            var info = game.Split(':');
+            var gameNumber = ExtractGameNumber(info[0]);
+            var counts = new CubeCount { R = 12, G = 13, B = 14 };
+            if (IsGamePossible(info[1], counts))
+                sum += gameNumber;
+        }
+
+        return sum.ToString();
+    }
+
+    private static int ExtractGameNumber(string game)
+    {
+        var number = 0;
+        for(var i = 0; i < game.Length; i++)
+        {
+            if (char.IsDigit(game[i]))
+            {
+                number *= 10;
+                number += game[i] - '0';
+            }
+        }
+        return number;
+    }
+
+    private static bool IsGamePossible(string game, CubeCount counts)
+    {
+        var gameData = game.Split(';');
+
+        foreach(var g in gameData)
+        {
+            var data = ExtractGameData(g);
+            if (!counts.CanContain(data))
+                return false;
+        }
+        return true;
+    }
+
+    private static CubeCount ExtractGameData(string game)
+    {
+        var count = new CubeCount();
+        var data = game.Split(',');
+        foreach(var d in data)
+        {
+            var gData = d.Trim().Split(' ');
+            var number = int.Parse(gData[0]);
+            var index = Array.IndexOf(_colors, gData[1]);
+            count[index] += number;
+        }
+        return count;
+    }
+
+    private static readonly string[] _colors = ["red", "green", "blue"];
+    private struct CubeCount
+    {
+        public int this[int i]
+        {
+            readonly get => i switch
+            {
+                0 => R,
+                1 => G,
+                2 => B,
+                _ => throw new IndexOutOfRangeException()
+            };
+            set
+            {
+                switch (i)
+                {
+                    case 0: R = value; break;
+                    case 1: G = value; break;
+                    case 2: B = value; break;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
+        public readonly bool CanContain(CubeCount other) => R >= other.R && B >= other.B && G >= other.G;
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -7,6 +7,12 @@ while(string.IsNullOrWhiteSpace(path) && !Directory.Exists(path))
     path = Console.ReadLine();
 }
 
+Console.WriteLine("Day One: Trebuchet");
 var dayOneInput = await File.ReadAllLinesAsync(Path.Combine(path, "DayOne.txt"));
 var dayOneSolution = Trebuchet.Evaluate(dayOneInput);
 Console.WriteLine(dayOneSolution);
+
+Console.WriteLine("Day Two: Cube Conundrum");
+var dayTwoInput = await File.ReadAllLinesAsync(Path.Combine(path, "DayTwo.txt"));
+var dayTwoSolution = CubeConundrum.Evaluate(dayTwoInput);
+Console.WriteLine(dayTwoSolution);
