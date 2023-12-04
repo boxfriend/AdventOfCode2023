da5dbd6a1db4916f5b6bbc776b326fae6b14804f
boxfriend <cawills394@gmail.com>
2023-12-04 00:05:04 -0500

    refactored to reduce repeated code

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -7,17 +7,16 @@ while(string.IsNullOrWhiteSpace(path) && !Directory.Exists(path))
     path = Console.ReadLine();
 }
 
-Console.WriteLine("Day One: Trebuchet");
-var dayOneInput = await File.ReadAllLinesAsync(Path.Combine(path, "DayOne.txt"));
-var dayOneSolution = Trebuchet.Evaluate(dayOneInput);
-Console.WriteLine(dayOneSolution);
+ProcessSolution<Trebuchet>("Day One: Trebuchet", Path.Combine(path, "DayOne.txt"));
+ProcessSolution<CubeConundrum>("Day Two: Cube Conundrum", Path.Combine(path, "DayTwo.txt"));
+ProcessSolution<GearRatios>("Day Three: Gear Ratios", Path.Combine(path, "DayThree.txt"));
 
-Console.WriteLine("Day Two: Cube Conundrum");
-var dayTwoInput = await File.ReadAllLinesAsync(Path.Combine(path, "DayTwo.txt"));
-var dayTwoSolution = CubeConundrum.Evaluate(dayTwoInput);
-Console.WriteLine(dayTwoSolution);
 
-Console.WriteLine("Day Three: Gear Ratios");
-var dayThreeInput = await File.ReadAllLinesAsync(Path.Combine(path, "DayThree.txt"));
-var dayThreeSolution = GearRatios.Evaluate(dayThreeInput);
-Console.WriteLine(dayThreeSolution);
+
+static async void ProcessSolution<T>(string message, string path) where T : IAdventSolution
+{
+    Console.WriteLine(message);
+    var input = await File.ReadAllLinesAsync(path);
+    var solution = T.Evaluate(input);
+    Console.WriteLine(solution);
+}
