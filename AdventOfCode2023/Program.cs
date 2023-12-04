4f5f8a40648d1b00da9c317cd442f2d35fe26528
boxfriend <cawills394@gmail.com>
2023-12-04 00:36:32 -0500

    whoops can't forget to await

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -7,16 +7,18 @@ while(string.IsNullOrWhiteSpace(path) && !Directory.Exists(path))
     path = Console.ReadLine();
 }
 
-ProcessSolution<Trebuchet>("Day One: Trebuchet", Path.Combine(path, "DayOne.txt"));
-ProcessSolution<CubeConundrum>("Day Two: Cube Conundrum", Path.Combine(path, "DayTwo.txt"));
-ProcessSolution<GearRatios>("Day Three: Gear Ratios", Path.Combine(path, "DayThree.txt"));
+await ProcessSolution<Trebuchet>("Day One: Trebuchet", Path.Combine(path, "DayOne.txt"));
+await ProcessSolution<CubeConundrum>("Day Two: Cube Conundrum", Path.Combine(path, "DayTwo.txt"));
+await ProcessSolution<GearRatios>("Day Three: Gear Ratios", Path.Combine(path, "DayThree.txt"));
+await ProcessSolution<ScratchCards>("Day Four: Scratchcards", Path.Combine(path, "DayFour.txt"));
 
 
 
-static async void ProcessSolution<T>(string message, string path) where T : IAdventSolution
+static async Task ProcessSolution<T>(string message, string path) where T : IAdventSolution
 {
     Console.WriteLine(message);
     var input = await File.ReadAllLinesAsync(path);
     var solution = T.Evaluate(input);
     Console.WriteLine(solution);
+    Console.WriteLine();
 }
