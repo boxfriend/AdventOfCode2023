78fbce227111211a012d0f57b8d799d38fd6dc82
boxfriend <cawills394@gmail.com>
2023-12-08 01:07:02 -0500

    day eight part one

---
+++ AdventOfCode2023/HauntedWasteland.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2023;
+internal class HauntedWasteland : IAdventSolution
+{
+    public static string Evaluate (string[] data)
+    {
+        var locations = new Dictionary<string, Directions>();
+        for(var i = 2; i < data.Length; i++)
+        {
+            var node = data[i][..3];
+            var dir = GetDirections(data[i][6..]);
+            locations.Add(node, dir);
+        }
+
+        var end = "ZZZ";
+        var current = "AAA";
+
+        var directions = data[0];
+        var steps = 0;
+        while(true)
+        {
+            var dir = directions[steps++ % directions.Length];
+            var node = locations[current];
+            if (dir == 'L')
+                current = node.Left;
+            else
+                current = node.Right;
+
+            if (current == end)
+                break;
+        }
+
+        return steps.ToString();
+    }
+
+    private static Directions GetDirections (string line) => new(line[1..4], line[6..9]);
+    private record struct Directions(string Left, string Right);
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -14,6 +14,7 @@ await ProcessSolution<ScratchCards>("Day Four: Scratchcards", Path.Combine(path,
 await ProcessSolution<SeedFertilizer>("Day Five: If You Give A Seed A Fertilizer", Path.Combine(path, "DayFive.txt"));
 await ProcessSolution<WaitForIt>("Day Six: Wait For It", Path.Combine(path, "DaySix.txt"));
 await ProcessSolution<CamelCards>("Day Seven: Camel Cards", Path.Combine(path, "DaySeven.txt"));
+await ProcessSolution<HauntedWasteland>("Day Eight: Haunted Wasteland", Path.Combine(path, "DayEight.txt"));
 
 
 
